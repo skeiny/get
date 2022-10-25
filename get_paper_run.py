0252afa028150632ea8bf1dfa8b4e0c6cd6930c1
sky <mad_sky_scau@163.com>
2022-10-25 15:05:02 +0800

    update

--- get_paper_run.py
+++ get_paper_run.py
@@ -8,28 +8,29 @@ from urllib.error import HTTPError
 
 
 def main():
-    ## config
-    key_words = ["csd","computational"]
-    conference_list = ["date",'dac','hpca','fast']
-    journal_list = [["tcad",41]]
+    # config
+    key_words = ['learned index', 'learned', 'index', 'LSM', 'KV', 'secondary', 'correlation', 'database']
+    conference_list = ['DAC', 'DATE', 'usenix', 'HPCA', 'FAST', 'SIGMOD', 'VLDB', 'ICDE', 'codes']
+    journal_list = [['tcad', 41], ['tpds', 33], ['tc', 71], ['tos', 18]]
 
+    if True:
+        loop_conference(conference_list, key_words, 2018, 2022)
+    if True:
+        loop_journal(journal_list, key_words, 5)
 
-    if(True):
-        loop_conference(conference_list, key_words, 2020, 2022)
-    if(True):
-        loop_journal(journal_list,key_words,3)
 
 def loop_conference(conference_list, key_words, begin, end):
     # get
     total = []
     for conf_name in conference_list:
         year = begin
-        while (year <= end):
+        while year <= end:
             try:
                 papers = getConference(conf_name.lower(), year)
             except Exception as e:
                 print(e)
-                break
+                year += 1
+                continue
             for i in papers:
                 total.append(i)
             year += 1
@@ -40,7 +41,7 @@ def loop_conference(conference_list, key_words, begin, end):
         save = False
         for word in key_words:
             if word.lower() in r["title"].lower():
-                # print(r["title"])
+                print(r["title"])
                 save = True
                 break
         if not save:
@@ -51,20 +52,23 @@ def loop_conference(conference_list, key_words, begin, end):
         os.makedirs(os.path.abspath(os.path.dirname(sys.argv[0]) + "/result"))
     os.chdir(os.path.abspath(os.path.dirname(sys.argv[0]) + "/result"))
     filename = str(datetime.date.today()) + "_ConfResult_" + str(begin) + "~" + str(end)
+    if os.path.exists(filename + ".csv"):
+        os.remove(filename + ".csv")
     df.to_csv((filename + ".csv"), index=False, encoding='utf_8_sig')
 
 
-def loop_journal(journal_list,key_words,loop):
+def loop_journal(journal_list, key_words, loop):
     # get
     total = []
     for journal in journal_list:
         volume = journal[1] - loop + 1
-        while (volume <= journal[1]):
+        while volume <= journal[1]:
             try:
                 papers = getJournals(journal[0].lower(), volume)
             except Exception as e:
                 print(e)
-                break
+                volume += 1
+                continue
             for i in papers:
                 total.append(i)
             volume += 1
@@ -75,7 +79,7 @@ def loop_journal(journal_list,key_words,loop):
         save = False
         for word in key_words:
             if word.lower() in r["title"].lower():
-                # print(r["title"])
+                print(r["title"])
                 save = True
                 break
         if not save:
@@ -86,6 +90,8 @@ def loop_journal(journal_list,key_words,loop):
         os.makedirs(os.path.abspath(os.path.dirname(sys.argv[0]) + "/result"))
     os.chdir(os.path.abspath(os.path.dirname(sys.argv[0]) + "/result"))
     filename = str(datetime.date.today()) + "_JournalResult"
+    if os.path.exists(filename + ".csv"):
+        os.remove(filename + ".csv")
     df.to_csv((filename + ".csv"), index=False, encoding='utf_8_sig')
 
 
@@ -141,6 +147,7 @@ def getJournals(name, volume):
             # paper["auth_url"] = auth_urls
             papers.append(paper)
     return papers
+
 
 # run here
 main()
